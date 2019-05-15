bb97fa06a50ae97c2bddbd74496e24b9c9ae2e61
mhamilt <matt.hamilton@ed.ac.uk>
2019-05-15 15:46:54 +0100

    add cpp-cli-audio-tools

--- FD-Class-Example/FD-Class-Example/main.cpp
+++ FD-Class-Example/FD-Class-Example/main.cpp
@@ -5,13 +5,13 @@
 //  Created by mhamilt on 20/10/2017.
 //  Copyright © 2017 mhamilt. All rights reserved.
 //
-//	Test the Plate Class is working correctly
+//    Test the Plate Class is working correctly
 
 #include <iostream>
 #include <string>
 #include "../../FDTDClasses/FDPlate.cpp"
 #include "../../FDTDClasses/FDString.cpp"
-#include "/../../cpp-cli-audio-tools/src/CliAudioTools.h"
+#include "../../cpp-cli-audio-tools/src/CliAudioTools.h"
 #include "../../CliFileNaming/CliFileName.h"
 
 //==============================================================================
@@ -23,20 +23,21 @@ int main (int argc, const char *argv[])
     // Sampling and Duration
     
     const double sampleRate = 44.1e3;
-    const double duration = 4;	     // duration (seconds)
+    const double duration = 4;         // duration (seconds)
     
     //==========================================================================
     // Set Output File Name
     
     const char *fileName = "/Downloads/Plate.wav";
     char *outputfname = CliSetFilename(argv, fileName);
+    WavCodec wavCodec;
     
     //==========================================================================
     // Plate Setup
     FDPlate::PlateParameters plateParams;
-    plateParams.t60 = .3;
+    plateParams.t60 = 10.3;
     plateParams.thickness = 0.001;
-    plateParams.tone = .5;
+    plateParams.tone = .9;
     plateParams.lengthX = .2;
     plateParams.lengthY = .2;
     plateParams.bcType = FDPlate::BoundaryCondition::simplySupported;
@@ -67,9 +68,9 @@ int main (int argc, const char *argv[])
     
     //==========================================================================
     // Output
-    writeWavMS(out, outputfname, Nf, sampleRate);
-    playWavMS(outputfname);
+    wavCodec.writeWavMS(out, outputfname, Nf, sampleRate);
     printf("\nComplete...\n");
     std::cout << "SUCCESS" << '\n';
     return EXIT_SUCCESS;
 }
+
